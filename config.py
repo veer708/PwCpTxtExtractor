71c7def9d9bf525cdcf30b9e8b591aa4e3478142
trishi880988 <gauravyt8803@gmail.com>
2025-04-27 10:41:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "7768725383:AAFOaTqDmuFQw2dAdXlgJH-O760qYfDZyc8"
-auth_users = [5840594311]
+api_id = "23237831"
+api_hash = "017dc661ca1432ac2fe5ecb62499d88f"
+bot_token = "8004864921:AAGnobEQ-UkBiQB_6HMcOb3xBf70ZndDnPM"
+auth_users = [6103594386]
