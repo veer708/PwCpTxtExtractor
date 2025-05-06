9b3c33803484aa5db8cad86922435d712f061260
trishi880988 <gauravyt8803@gmail.com>
2025-05-06 19:50:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ log_channel = -1001234567890  # Replace with your log channel ID
 print(4321)
 bot = Client(
     "bot",
-    api_id=017dc661ca1432ac2fe5ecb62499d88f,
+    api_id="017dc661ca1432ac2fe5ecb62499d88f",
     api_hash=23237831,
     bot_token=7805274671:AAHde-i_R0z0zNZ5_BrylKD9gYnT2iv9Mzo)
 
@@ -1661,4 +1661,3 @@ async def send_to_log_channel(bot, content):
         )
     except Exception as e:
         logging.error(f"Failed to send to log channel: {e}")
-
