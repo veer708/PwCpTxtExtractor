eda0f3a88c25d19e10dbbb6cb8094570d67a32ce
trishi880988 <gauravyt8803@gmail.com>
2025-05-06 19:39:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,9 +49,9 @@ log_channel = -1001234567890  # Replace with your log channel ID
 print(4321)
 bot = Client(
     "bot",
-    api_id=api_id,
-    api_hash=api_hash,
-    bot_token=bot_token)
+    api_id=017dc661ca1432ac2fe5ecb62499d88f,
+    api_hash=23237831,
+    bot_token=7805274671:AAHde-i_R0z0zNZ5_BrylKD9gYnT2iv9Mzo)
 
 @bot.on_message(filters.command(["start"]))
 async def start(bot, message):
