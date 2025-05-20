acccd5e795600e3aa2f426b50e0151f9f8c105ce
veer708 <veerahirwal51@gmail.com>
2025-05-20 16:44:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "29232353"
-api_hash = "6868788228291767c90e4346eea03f36"
+api_id = "27451385"
+api_hash = "6a44b4c75073c5c0452ac2f792b2c367"
 bot_token = ""
-auth_users = [6650855788]
+auth_users = [8066969842]
